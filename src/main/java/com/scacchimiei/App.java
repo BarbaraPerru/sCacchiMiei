80f82b6fafbf0f199f5c0bdd1d8ff94cb7fc0e03
Barbara Perruci <barbara.perrucci1987@gmail.com>
2025-07-14 16:21:30 +0200

    GamePanel class created, adding  piecies in resource folder

--- src/main/java/com/scacchimiei/App.java
+++ src/main/java/com/scacchimiei/App.java
@@ -1,13 +1,24 @@
 package com.scacchimiei;
 
-/**
- * Hello world!
- *
- */
+import javax.swing.JFrame;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // Creating the screen
+        JFrame window = new JFrame("Chess");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        // Add GamePanel to the screen
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gp.launchGame();
+
     }
 }
